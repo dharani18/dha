0a9aecc46bc35962c2a36be31e1daf7d93920c4a
dharani18 <36472689+dharani18@users.noreply.github.com>
2018-03-04 10:31:09 +0530

    Create evod.cpp

---
+++ evod.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    
+    cout << "Enter a and b values: ";
+    cin >> a>>b;
+    {
+        c+a+b;
+        if(c%2==0)
+        cout<<"even";
+        else
+        cout<<"odd";
+    }
+
+    return 0;
+}
