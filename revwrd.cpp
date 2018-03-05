19b714f5d94ff2388b68b8ae73b9e7d2616fedc3
dharani18 <36472689+dharani18@users.noreply.github.com>
2018-03-05 21:37:14 +0530

    Create revwrd.cpp

---
+++ revwrd.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+        string s;
+        int begin,end,i,j=0,len,temp,count=0;
+ 
+        cout<<"ENTER STRING: ";
+        getline(cin,s);
+        len=s.length(); 
+        for(i=0;i<len;i++)
+        {
+                 if(s[i]==' ' || s[i]=='\0')
+                {
+                         for(begin=j,end=i-1 ; begin<(i+j)/2 ; begin++,end--)
+                         {
+                                   temp=s[begin];
+                                   s[begin]=s[end];
+                                   s[end]=temp;
+                         }
+                         j=i+1;
+                }
+         }
+ 
+       cout<<s<<" ";
+ 
+       return 0;
+}
