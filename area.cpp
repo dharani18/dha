31025fc9d355d507b92978ac4ed493f5aca70d5f
dharani18 <36472689+dharani18@users.noreply.github.com>
2018-03-13 19:46:14 +0530

    Create area.cpp

---
+++ area.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int length,breath,area_rec=0;
+cout<<"Enter Length and Breath values: ";
+cin>>length>>breath;
+area_rec=length*breath;
+cout<<"\nArea of Ractangle: "cout<<area_rec;
+return 0;
+}
