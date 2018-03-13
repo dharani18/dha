70d433f22c8cc12c405b74c5f19ab5e30bee2c9b
dharani18 <36472689+dharani18@users.noreply.github.com>
2018-03-13 20:13:50 +0530

    Create lcm.cpp

---
+++ lcm.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+void lcm(int,int);
+
+int main()
+{
+    int a,b;
+    clrscr();
+    cout<<"Enter two numbers: ";
+    cin>>a;
+    cin>>b;
+    lcm(a,b);
+}
+void lcm(int a,int b)
+{
+    int m,n;
+    m=a;
+    n=b;
+   while(m!=n)
+    {
+	if(m < n)
+	{
+	m=m+a;
+	}
+	else
+	{
+	    n=n+b;
+	    }
+    }
+
+    cout<<"\nL.C.M of "<<a<<" and "<<b<<" is "<<m;
+    return 0;
+}
