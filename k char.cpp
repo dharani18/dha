3de044491d27fcf486a25f2f8a46105ba22f932b
dharani18 <36472689+dharani18@users.noreply.github.com>
2018-03-19 20:31:13 +0530

    Create k char.cpp

---
+++ k char.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+char str[];
+cout<<"enter the string:";
+cin>>str;
+cout<<"enter k:";
+cin>>k;
+for(i=0;i<k;i++)
+{
+cout<<str;
+}
+return 0;
+}
