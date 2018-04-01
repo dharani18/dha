47997a9d889a338058a99fbdf51db3a6b5ed5972
dharani18 <36472689+dharani18@users.noreply.github.com>
2018-04-01 13:11:46 +0530

    Create ddiff.cpp

---
+++ ddiff.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    long a,g;
+    while(cin>>a>>g)
+    {
+        cout<<" diffrence :" <<labs(a-g)<<endl;
+        
+    }
+
+    return 0;
+}
