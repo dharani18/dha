9ae1a4f1eb6e05f1d74826a8961aaf7ddf9e84e6
dharani18 <36472689+dharani18@users.noreply.github.com>
2018-05-23 14:54:55 +0530

    Create pooow.cpp

---
+++ pooow.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+int b,p,c;
+cin>>b>>p;
+if((b>0)||(p=0))
+{ 
+	cout<<"1";
+}
+else if((b>0)||(p>0))
+{
+ c=pow(b,p);
+ cout<<c;
+ }
+ else
+ { 
+ 	cout<<"invalid:";
+ }
+ return 0;
+}
