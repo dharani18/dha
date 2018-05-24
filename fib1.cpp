2cf210d372461ae15b856264c537489271afe7a5
dharani18 <36472689+dharani18@users.noreply.github.com>
2018-05-24 16:12:34 +0530

    Create fib1.cpp

---
+++ fib1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+#include<stdlib.h>
+using namespace std;
+
+int main()
+{
+   int f1=0,f2=1,f3;
+   int i,num;
+   cin>>num;
+   cout<<f2<<" ";
+   for(i=0;i<num-1;i++)
+   {
+       f3=f1+f2;
+       f1=f2;
+       f2=f3;
+       cout<<f3<<" ";
+   }
+   return 0;
+}
