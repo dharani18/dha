3ef23f3b3563e3624ce34de13eabab653101ddd4
dharani18 <36472689+dharani18@users.noreply.github.com>
2018-05-24 11:10:02 +0530

    Create maxarray.cpp

---
+++ maxarray.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+ int n,i;
+ float array[100];
+ cin>>n;
+ if((n>0)||(n<0))
+ {
+ for(i=0;i<n;i++)
+ { 
+           cin>>array[i];
+ }
+ for(i=1;i<n;i++)
+ {
+ 	if(array[0]<array[i])
+            array[0]=array[i];
+ }
+ cout<<"largest element="<<array[0];
+ }
+ else
+ { 
+ 	cout<<"invalid";
+ }
+ return 0;
+}
