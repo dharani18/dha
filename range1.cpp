828b5b22904164997a5c65600cdafc1df07fbcd8
dharani18 <36472689+dharani18@users.noreply.github.com>
2018-05-25 12:24:33 +0530

    Create range1.cpp

---
+++ range1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+   int n;
+   cin>>n;
+   if((n>0)&&(n<=10))
+       {
+       cout<<"yes";
+       }
+   else
+   {
+       cout<<"no";
+   }
+   return 0;
+}
