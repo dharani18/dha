b444a2875dcf5204d70c9ff8e037940f2f79b40f
dharani18 <36472689+dharani18@users.noreply.github.com>
2018-05-25 11:50:43 +0530

    Create k1times.cpp

---
+++ k1times.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{ 
+    string S;
+    int i,k;
+    cin>>S;
+    cin>>k;
+    for(i=0;i<k;i++)
+    {
+        cout<<S<<endl;
+    }
+    return 0;
+}
+    
